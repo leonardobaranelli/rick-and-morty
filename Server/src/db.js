0a189e5a8f086b0ac77523ea9ebc5175a9eae955
Leonardo A Baranelli <leobaranelli@gmail.com>
2023-09-26 16:37:28 -0300

    feat: sequelize db connection

---
+++ Server/src/db.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+const { Sequelize } = require("sequelize");
+
+const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+
+const sequelize = new Sequelize(
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/rickandmorty`,
+  { logging: false, native: false }
+);
+
+module.exports = {
+  conn: sequelize,
+};
