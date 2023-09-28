2badf64a79d4613a381417ccc7ceb0dff6859e5d
Leonardo A Baranelli <leobaranelli@gmail.com>
2023-09-28 19:48:17 -0300

    feat: getLogin controller (and route)

--- Server/src/routes/index.js
+++ Server/src/routes/index.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 const getCharById  = require('../controllers/getCharById')
+const getLogin = require("../controllers/getLogin");
 const postUser = require("../controllers/postUser");
 
 router.get("/character/:id", getCharById);
+router.get("/login", getLogin);
 router.post("/login", postUser);
 
 module.exports = router;
