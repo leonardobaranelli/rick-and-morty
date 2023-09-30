8e3222ad4672a08eb5a2db4f28d6a9e6c17eb6b0
leonardobaranelli <leobaranelli@gmail.com>
2023-09-30 07:21:39 -0300

    feat: About component

---
+++ Client/src/components/About/About.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import style from './About.module.css';
+
+const About = () => {
+    return(
+        <div>
+            Welcome to the portal of interdimensional adventures! I present this Rick and Morty application, where you can explore the fascinating universe of these characters.
+
+            Prepare for an unforgettable journey through the digital planes of the multiverse!
+        </div>
+    );
+}
+
+export default About;
