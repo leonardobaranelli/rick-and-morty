5fde5f8f85f47a59c422096c862cee228f9574bb
leonardobaranelli <leobaranelli@gmail.com>
2023-10-09 15:05:21 -0300

    feat: validations on CreateUser

--- Client/src/views/CreateUser/CreateUser.jsx
+++ Client/src/views/CreateUser/CreateUser.jsx
@@ -1,34 +1,44 @@
 import style from "./CreateUser.module.css";
 import { useState } from "react";
+import validations from "./validations";
 
-const CreateUser = () => {
+const CreateUser = ({login}) => {
     
     const [userData, setUserData] = useState({
         email: "",
         password: "",
     });
     
+    const [errors, setErrors] = useState({
+        email: "",
+        password: "",
+    })
+
     const handleChange = (event) => {
         const property = event.target.name;
         const value = event.target.value;
     
-        setUserData({...userData, [property] : value});           
+        setUserData({...userData, [property] : value});          
+        validations({...userData, [property] : value}, errors, setErrors); 
     }
 
     const submitHandler = (event) => {
         event.preventDefault();
         console.log("all ok")
+        //login(userData);
     }
     
     return(
         <form onSubmit={submitHandler}>
             <div>
                 <label htmlFor="email">Email:</label>
-                <input name="email" type="text" onChange={handleChange}></input>                
+                <input name="email" type="text" onChange={handleChange}></input>     
+                <p>{errors.email}</p>           
             </div>
             <div>
                 <label htmlFor="password">Password:</label>
                 <input name="password" type="text" onChange={handleChange}></input>                
+                <p>{errors.password}</p>           
             </div>
             <button>Submit</button>
         </form>
