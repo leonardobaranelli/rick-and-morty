c0ffb6b1237b371636a747664f3bd0fbb6f6532a
leonardobaranelli <leobaranelli@gmail.com>
2023-12-13 18:30:21 -0300

    style: NavBar component

--- Client/src/components/NavBar/NavBar.jsx
+++ Client/src/components/NavBar/NavBar.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import style from "./NavBar.module.css";
-import { Link } from "react-router-dom";
+import { Link } from "react-router-dom"; 
 
 const Nav = () => {   
    return (      
-      <nav>          
-         <Link to ="/about">About</Link><br/>
-         <Link to ="/home">Home</Link><br/>         
-         <Link to ="/favorites">Favorites</Link><br/>
-         <Link to ="/createUser">Create User</Link>
+      <nav className={style.mainContainer}>          
+         <Link to ="/home">HOME</Link><br/>         
+         <Link to ="/favorites">FAVORITES</Link><br/>
+         <Link to ="/createUser">CREATE USER</Link>
+         <Link to ="/about">ABOUT</Link><br/>
       </nav>      
    );
 }
