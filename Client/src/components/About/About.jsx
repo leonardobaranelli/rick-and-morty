41dc500085c21f9984b92c3852c63c39bf8bf5f3
leonardobaranelli <leobaranelli@gmail.com>
2023-12-15 07:44:05 -0300

    style: About component

--- Client/src/components/About/About.jsx
+++ Client/src/components/About/About.jsx
@@ -3,9 +3,9 @@ import style from './About.module.css';
 
 const About = () => {
     return(
-        <div>
+        <div className={style.mainContainer}> 
             Welcome to the portal of interdimensional adventures! I present this Rick and Morty application, where you can explore the fascinating universe of these characters.
-
+            <br/><br/>
             Prepare for an unforgettable journey through the digital planes of the multiverse!
         </div>
     );
