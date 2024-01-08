2d74d82e9231a8f4cc06ea727d237014681ce937
leonardobaranelli <leobaranelli@gmail.com>
2024-01-08 19:18:17 -0300

    fix: CORS issue

--- Server/src/app.js
+++ Server/src/app.js
@@ -1,21 +1,22 @@
 const express = require('express');
 const morgan = require('morgan');
 
-const router = require('./routes/index')
+const router = require('./routes/index');
 const app = express();
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  const origin = req.headers.origin;
+
+  res.header('Access-Control-Allow-Origin', origin);
   res.header('Access-Control-Allow-Credentials', 'true');
-  res.header(
-     'Access-Control-Allow-Headers',
-     'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  res.header(
-     'Access-Control-Allow-Methods',
-     'GET, POST, OPTIONS, PUT, DELETE'
-  );
-  next();
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
 });
 
 app.use(express.json());
